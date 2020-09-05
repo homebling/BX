069e389f402f90e9601671f1dcdafe0641202e10
homebling <1056063810@qq.com>
2020-09-05 17:30:55 +0800

    change py

--- BX.py
+++ BX.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# 读取三表数据
 books = pd.read_csv('BX-Books.csv', sep=';', error_bad_lines=False, encoding="latin-1")
 books.columns = ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'imageUrlS', 'imageUrlM', 'imageUrlL']
 users = pd.read_csv('BX-Users.csv', sep=';', error_bad_lines=False, encoding="latin-1")
@@ -12,43 +13,47 @@ users.columns = ['userID', 'Location', 'Age']
 ratings = pd.read_csv('BX-Book-Ratings.csv', sep=';', error_bad_lines=False, encoding="latin-1")
 ratings.columns = ['userID', 'ISBN', 'bookRating']
 
+# 统计评分表
 print(ratings.shape)
 print(list(ratings.columns))
+print(ratings.head())
 
-ratings.head()
-
+'''# 根据评分画图
 plt.rc("font", size=15)
 ratings.bookRating.value_counts(sort=False).plot(kind='bar')
 plt.title('Rating Distribution\n')
 plt.xlabel('Rating')
 plt.ylabel('Count')
 plt.savefig('system1.png', bbox_inches='tight')
-plt.show()
+plt.show()'''
 
+# 统计书籍表和用户表
 print(books.shape)
 print(list(books.columns))
-
-
-books.head()
+print(books.head())
 
 print(users.shape)
 print(list(users.columns))
+print(users.head())
 
-users.head()
-
+'''# 年龄分段统计用户数量
 users.Age.hist(bins=[0, 10, 20, 30, 40, 50, 100])
 plt.title('Age Distribution\n')
 plt.xlabel('Age')
 plt.ylabel('Count')
 plt.savefig('system2.png', bbox_inches='tight')
-plt.show()
+plt.show()'''
 
+# 根据ISBN统计每本书的总评分
 rating_count = pd.DataFrame(ratings.groupby('ISBN')['bookRating'].count())
-rating_count.sort_values('bookRating', ascending=False).head()
+print(rating_count.sort_values('bookRating', ascending=False).head())
 
-most_rated_books = pd.DataFrame(['0971880107', '0316666343', '0385504209', '0060928336', '0312195516'], index=np.arange(5), columns = ['ISBN'])
+
+most_rated_books = pd.DataFrame(['0971880107', '0316666343', '0385504209', '0060928336', '0312195516'], index=np.arange(5), columns=['ISBN'])
+print(most_rated_books)
 most_rated_books_summary = pd.merge(most_rated_books, books, on='ISBN')
-most_rated_books_summary
+print(most_rated_books_summary)
+'''
 
 average_rating = pd.DataFrame(ratings.groupby('ISBN')['bookRating'].mean())
 average_rating['ratingCount'] = pd.DataFrame(ratings.groupby('ISBN')['bookRating'].count())
@@ -75,5 +80,5 @@ books_corr_to_bones = pd.DataFrame(['0312291639', '0316601950', '0446610038', '0
                                   index=np.arange(9), columns=['ISBN'])
 corr_books = pd.merge(books_corr_to_bones, books, on='ISBN')
 corr_books
-
+'''
 
